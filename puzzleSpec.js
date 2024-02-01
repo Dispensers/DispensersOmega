ee0636127c2043283c00792bf7fdf9040067d629
Dispensers <121032668+Dispensers@users.noreply.github.com>
2024-02-01 19:19:18 +0000

    Add files via upload

--- puzzleSpec.js
+++ puzzleSpec.js
@@ -1,3 +1,55 @@
+
+let punterPuzzleSpec = {
+	number: "B2",
+	solveBy: "7 Feb",
+	dispenserSpec: [undefined, "pq", "rq", "ppprqq"],
+	ringSpec: [-1, -4, 7, -2, 5, 8, -3, 6, 9],
+	colourSpec: ["blue", "green", "black"],
+	hintSpec: ["blue", 5],
+	solutionDispenseSequence: [3, 3, 3, 3, 3, 2, 2, 1, 3],
+	solutionColourSequence: ["green", "blue", "black", "green", "blue", "green", "black", "green", "blue"]
+};
+
+
+
+
+/*
+let punterPuzzleSpec = {
+	number: "F1",
+	solveBy: "28 Jan",
+	dispenserSpec: [undefined, "pqrsr", "srrqp"],
+	ringSpec: [8, 6, 4, -2, 9, -1, -3, 5, 7],
+	colourSpec: ["blue", "orange", "black", "green"],
+	hintSpec: ["green", 6],
+	solutionDispenseSequence: [3, 2, 3, 3, 3, 3, 1, 3, 3],
+	solutionColourSequence: ["black", "green", "black", "orange", "blue", "green", "blue", "green", "blue"]
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: "B1",
+	solveBy: "28 Jan",
+	dispenserSpec: [undefined, "ppqrq", "rpqr"],
+	ringSpec: [-1, -4, 7, -2, 5, 8, -3, 6, 9],
+	colourSpec: ["black", "green", "orange"],
+	hintSpec: ["green", 6],
+	solutionDispenseSequence: [3, 2, 3, 3, 3, 3, 1, 3, 3],
+	solutionColourSequence: ["black", "green", "black", "orange", "blue", "green", "blue", "green", "blue"]
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: "A2",
+	solveBy: "28 Jan",
+	dispenserSpec: [undefined, "pqppqq", "rpq", "r"],
+	ringSpec: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+	colourSpec: ["orange", "black", "blue"],
+	hintSpec: ["green", 6],
+	solutionDispenseSequence: [3, 2, 3, 3, 3, 3, 1, 3, 3],
+	solutionColourSequence: ["black", "green", "black", "orange", "blue", "green", "blue", "green", "blue"]
+};
+*/
+/*
 let punterPuzzleSpec = {
 	number: 1,
 	solveBy: "28 Jan",
@@ -16,5 +68,5 @@ let punterPuzzleSpec = {
 	solutionDispenseSequence: [3, 2, 3, 3, 3, 3, 1, 3, 3],
 	solutionColourSequence: ["black", "green", "black", "orange", "blue", "green", "blue", "green", "blue"]
 };
-
+*/
 
